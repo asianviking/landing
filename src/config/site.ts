d828b2ddebfb62c1990b8a8747fa858b4d9c728c
ayvee <asianvikingofficial@gmail.com>
2023-11-24 00:01:49 +0700

    feat: add initial ui

---
+++ src/config/site.ts
@@ -0,0 +1,13 @@
+import { SiteConfig } from "types"
+
+export const siteConfig: SiteConfig = {
+  name: "Taxonomy",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "https://tx.ayvee.dev",
+  ogImage: "https://tx.ayvee.dev/og.jpg",
+  links: {
+    twitter: "https://x.com/ayvee_dev",
+    github: "https://github.com/asianviking/taxonomy",
+  },
+}
